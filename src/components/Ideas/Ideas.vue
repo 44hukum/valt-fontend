da954fac37c4c5cbb271e22c8febe66636a8d167
Prajan Shrestha <prajan@mavorion.com>
2022-09-26 11:05:08 +0545

    style

--- src/components/Ideas/Ideas.vue
+++ src/components/Ideas/Ideas.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container d-flex">
+  <div class="container d-flex" style="justify-content: center;">
     <template v-for="idea in ideas" :key="idea.id">
-        <a-card :title="idea.title" style="width: 300px; margin: 0 30px;">
+        <a-card :title="idea.title" style="width: 300px; margin: 0 10px;">
             <p>{{ idea.body }}</p>
         </a-card>
     </template>
