25825f158f567e79bb893275b0d5839f7a076e30
Prajan Shrestha <prajanshrestha10@gmail.com>
2022-10-19 09:45:11 +0545

    subtle changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,23 @@
     <router-link :to="{ name: 'Home' }">
         <img src="../assets/idea.png" alt="logo" style="width: 120px; margin-left: -10px" >
     </router-link>
-    <div class="row mt-2">
+    <div class="row" style="margin-top: 20px;">
         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem consequuntur minima placeat quas veritatis optio nisi doloribus architecto sequi vel temporibus suscipit numquam similique quisquam iusto, asperiores, sunt exercitationem maxime iure atque! Aperiam labore delectus, eligendi id veritatis rerum possimus voluptatibus aut dolorum non debitis et praesentium numquam illo accusamus libero suscipit similique? Officiis perferendis porro eum inventore quam molestiae accusamus explicabo tempore fugiat. Molestiae quae dolorum architecto consequuntur sint ad voluptatum veniam omnis maiores laudantium perspiciatis consectetur, sit neque voluptatem quidem recusandae est molestias perferendis deleniti nemo fugit minus aspernatur. Quas praesentium facere at esse! Fugiat odio eligendi officia?
-            <br>
+            <h1>WHERE THE WORLD WORKS</h1>
+            <p style="font-size: 1.25rem; margin-top: 15px;">An online marketplace that connects <br>freelancers and business owners around <br>the world with easy-to-do affordable tasks.</p>
             <router-link :to="{ name: 'Index' }">
-                <button class="my-3 btn btn-primary">Explore Ideas</button>
+                <button class="btn btn-primary my-3">Explore Ideas</button>
             </router-link>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem consequuntur minima placeat quas veritatis optio nisi doloribus architecto sequi vel temporibus suscipit numquam similique quisquam iusto, asperiores, sunt exercitationem maxime iure atque! Aperiam labore delectus, eligendi id veritatis rerum possimus voluptatibus aut dolorum non debitis et praesentium numquam illo accusamus libero suscipit similique? Officiis perferendis porro eum inventore quam molestiae accusamus explicabo tempore fugiat. Molestiae quae dolorum architecto consequuntur sint ad voluptatum veniam omnis maiores laudantium perspiciatis consectetur, sit neque voluptatem quidem recusandae est molestias perferendis deleniti nemo fugit minus aspernatur. Quas praesentium facere at esse! Fugiat odio eligendi officia?
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
         </div>
     </div>
   </div>
